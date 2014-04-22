cc93f19d850e7c4e4304bf5e28aae2ee5df9acea
zariganitosh <XXXX@example.com>
2014-04-22 11:03:44 +0900

    落ちるブロック

--- tetris.rb
+++ tetris.rb
@@ -1,12 +1,11 @@
 require "starruby"
 include StarRuby
 
-font = Font.new("fonts/ORANGEKI", 12)
 white = Color.new(255, 255, 255)
 
 y = 0
 x = 8
-Game.run(320, 240, :title => "Hello, World!") do |game|
+Game.run(320, 240, :title => "tetris") do |game|
   break if Input.keys(:keyboard).include?(:escape)
   x += 8 if Input.keys(:keyboard).include?(:right)
   x -= 8 if Input.keys(:keyboard).include?(:left)
@@ -15,5 +14,5 @@ Game.run(320, 240, :title => "Hello, World!") do |game|
   y = 0 if y > 240
 
   game.screen.clear
-  game.screen.render_text("Hello, World!", x, y, font, white)
+  game.screen.render_rect(x, y, 16, 16, white)
 end
