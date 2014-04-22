a0d362b83bd16d82ae20fa1781bb31ec1a7afbad
zariganitosh <XXXX@example.com>
2014-04-22 16:09:32 +0900

    テトリミノを回転させる

--- tetris.rb
+++ tetris.rb
@@ -54,7 +54,24 @@ class Tetrimino
   def initialize
     @id = rand(0..6)
     @blocks = @@minos[@id]
-    @x, @y = 3, 0
+    @x, @y, @angle = 3, 0, 0
+  end
+  
+  def blocks(angle = @angle)
+    case angle % 4
+    when 0
+      @blocks
+    when 1
+      @blocks.transpose.map(&:reverse)  #右90度回転
+    when 2
+      @blocks.reverse.map(&:reverse)    #180度回転
+    when 3
+      @blocks.transpose.reverse         #左90度回転（右270度回転）
+    end
+  end
+  
+  def rotate(dr)
+    @angle += dr
   end
   
   def side_step(dx)
@@ -73,11 +90,15 @@ Game.run(FIELD_W, FIELD_H, :title => "tetris") do |game|
   @tetrimino ||= Tetrimino.new
   dx = 0
   dy = 0.125
+  dr = 0
   
   break if Input.keys(:keyboard).include?(:escape)
   dx =  1 if Input.keys(:keyboard).include?(:right)
   dx = -1 if Input.keys(:keyboard).include?(:left)
+  dr =  1 if Input.keys(:keyboard).include?(:x)
+  dr =  3 if Input.keys(:keyboard).include?(:z)
 
+  @tetrimino.rotate(dr)
   @tetrimino.side_step(dx)
   @tetrimino.fall(dy)
   @tetrimino = nil if @tetrimino.y >= FIELD_ROW
